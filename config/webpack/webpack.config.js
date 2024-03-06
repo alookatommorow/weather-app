a67e3a1d2c0fcbb581ce57bde976cbbf1d3a6db9
John Hess <hessjohnt@gmail.com>
2024-03-06 15:06:10 -0800

    add dotenv for webpack

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,6 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig } = require('shakapacker')
+require('dotenv').config();
 
 const webpackConfig = generateWebpackConfig()
 
